95e0fa39bee273ba3bbdd52877aa8de7ae32494f
Ailton Ferreira Luz <djailtim@msn.com>
2023-03-07 21:34:35 -0300

    Initial commit

---
+++ src/main/java/com/example/projectbookstore/ProjectBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBookstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectbookstore/ProjectBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
