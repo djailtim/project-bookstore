7c1cacf5d90d1b7abe54e1011b9dd4f507aa1398
Ailton Ferreira Luz <djailtim@msn.com>
2023-03-10 20:37:04 -0300

    Initial implemented thymeleaf

---
+++ src/main/java/com/example/projectbookstore/controllers/BookRestController.java
@@ -0,0 +1,59 @@
+package com.example.projectbookstore.controllers;
+
+import com.example.projectbookstore.model.Book;
+import com.example.projectbookstore.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookRestController {
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping("/create")
+    public Book save(@RequestBody Book book) {
+        return bookService.save(book);
+    }
+
+    @GetMapping
+    public List<Book> findAll() {
+        return bookService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(bookService.findById(id));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<?> findByTitle(@RequestParam String title) {
+        try {
+            return ResponseEntity.ok(bookService.findByTitle(title));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/edit/{id}")
+    public ResponseEntity<?> edit(@PathVariable Long id, @RequestBody Book book) {
+        try {
+            return ResponseEntity.ok(bookService.edit(id, book));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        bookService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+}
